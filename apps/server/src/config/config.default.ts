590e0690e2501cc2701108c577bca06e964a3869
sunboquan <sunboquan@zhihu.com>
2024-07-01 19:44:53 +0800

    feat(*): 初始化前后端项目

---
+++ apps/server/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1719834242394_721',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
